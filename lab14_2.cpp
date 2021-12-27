9d4f4ee26d201110b8983b7c1b16152f43df27fa
Zeronos <boss16805@gmail.com>
2021-12-27 16:31:48 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,41 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool R[][M],int s,int x,int y){
+    int S;
+    if(s >= 1){
+        S = s;
+    }
+    for(int i = 0; i < 30; i++){
+        for(int j = 0; j < 70; j++){
+            double b = sqrt(pow(i-x,2)+pow(j-y,2));
+            if(b <= S-1){
+                R[i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool R[][M]){
+    for(int e = 0; e < 72 ; e++){
+        cout << "-";
+    }
+    cout << "\n";
+    for(int i = 0; i < 30; i++){
+        cout << "|";
+        for(int j = 0; j < 70; j++){
+            if(R[i][j] == 1){
+                cout << "*";
+            }
+            if(R[i][j] == 0){
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    for(int f = 0; f < 72 ; f++){
+        cout << "-";
+    }
+    cout << "\n";
+}
