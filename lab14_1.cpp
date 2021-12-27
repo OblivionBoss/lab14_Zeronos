6411a84c95b07a453e6f9e63d95eecfac93ddf73
Zeronos <boss16805@gmail.com>
2021-12-27 15:15:17 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,34 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0,sumdev = 0,geoval = 1,sumhar = 0;
+    double min = A[0],max = A[0],invpw = (1.0/N);
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+    }
+    B[0] = sum / N;
+    for(int j = 0; j < N; j++){
+        sumdev += pow(A[j] - B[0],2);
+    }
+    B[1] = sqrt(sumdev / N);
+    for(int k = 0; k < N; k++){
+        geoval = geoval*A[k];
+    }
+    B[2] = pow(geoval,invpw);
+    for(int l = 0; l < N; l++){
+        sumhar += (1/A[l]);
+    }
+    B[3] = N / sumhar;
+    for(int m = 0; m < N; m++){
+        if(A[m] > max){
+            max = A[m];
+        }
+        if(A[m] < min){
+            min = A[m];
+        }
+    }
+    B[4] = max;
+    B[5] = min;
+}
